a0c7a378573061a7aed262cd4f056b46c3c03f09
NguyenHuuThang10 <nguyenhuuthangag123@gmail.com>
2024-01-31 22:46:54 +0700

    sort middlewares

--- src/app/controlers/MeController.js
+++ src/app/controlers/MeController.js
@@ -4,8 +4,16 @@ class MeController {
 
   // [GET] /me/stored/courses
   storedCourses(req, res, next) {
+    let courseQuery = Course.find({})
 
-    Promise.all([Course.find({}), Course.countDocumentsWithDeleted({ deleted: true })])
+    if (req.query.hasOwnProperty('_sort')){
+      courseQuery = courseQuery.sort({
+        [req.query.column]: req.query.type
+      })
+    }
+    
+
+    Promise.all([courseQuery, Course.countDocumentsWithDeleted({ deleted: true })])
       .then(([courses, deleteCount]) => {
           res.render('me/stored-courses', {
             deleteCount,
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const morgan = require("morgan");
 const { engine } = require("express-handlebars");
 const methodOverride = require('method-override'); // thư viện chuyển POST thành PUT, DELETE
+const SortMiddleware = require('./app/middlewares/SortMiddleware')
 const app = express();
 const port = 3000;
 
@@ -24,6 +25,9 @@ app.use(express.json());
 
 app.use(methodOverride('_method'))
 
+//Custom middlewares
+app.use(SortMiddleware)
+
 // XMLHttprequest, fethc, axios
 
 //HTTP logger
@@ -34,6 +38,28 @@ app.engine("hbs", engine({
   extname: ".hbs",
   helpers: {
     sum: (a, b) => a+b,
+    sortable: (field, sort) => {
+      const sortType = field === sort.column ? sort.type : 'default'
+
+      const icons = {
+        default: 'oi oi-elevator',
+        asc: 'oi oi-sort-ascending',
+        desc: 'oi oi-sort-descending'
+      }
+
+      const types = {
+        default: 'desc',
+        asc: 'desc',
+        desc: 'asc'
+      }
+
+      const icon = icons[sortType]
+      const type = types[sortType]
+      return `<a href="?_sort&column=${field}&type=${type}">
+                <span class="${icon}"></span>
+            </a>`
+
+    }
   }
 }));
 app.set("view engine", "hbs");
